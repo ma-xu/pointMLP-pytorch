5cb88d65e357f978322566cb2ca75f12b3567ce3
Xu Ma <xuma@my.unt.edu>
2022-04-19 15:45:58 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -5,9 +5,10 @@ from fvcore.nn import FlopCountAnalysis
 from classification_ScanObjectNN.models import pointMLPElite
 
 model = pointMLPElite()
+model.eval()
 # model = deit_tiny_patch16_224()
 
-inputs = (torch.randn((1,3,1024)),)
+inputs = (torch.randn((1,3,1024)))
 k = 1024.0
 flops = FlopCountAnalysis(model, inputs).total()
 print(f"Flops : {flops}")
